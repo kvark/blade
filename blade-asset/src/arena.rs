0361c65e49edebbb9db1e75e50a6e08fe8c0921e
Dzmitry Malyshau <kvark@fastmail.com>
2023-04-10 23:03:40 -0700

    First steps at the asset manager

--- blade-asset/src/arena.rs
+++ blade-asset/src/arena.rs
@@ -1,7 +1,7 @@
 use std::{
     marker::PhantomData,
-    num::NonZeroU32,
-    ops,
+    num::NonZeroU8,
+    ops, ptr,
     sync::{
         atomic::{AtomicPtr, Ordering},
         Mutex,
@@ -9,12 +9,19 @@ use std::{
 };
 
 #[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Hash)]
 struct Address {
-    index: NonZeroU32,
-    chunk: u8,
+    index: u32,
+    chunk: NonZeroU8,
 }
 
 pub struct Handle<T>(Address, PhantomData<T>);
+impl<T> Clone for Handle<T> {
+    fn clone(&self) -> Self {
+        Handle(self.0, PhantomData)
+    }
+}
+impl<T> Copy for Handle<T> {}
 
 const MAX_CHUNKS: usize = 30;
 
@@ -25,7 +32,7 @@ struct FreeManager<T> {
 }
 
 pub struct Arena<T> {
-    init_size: usize,
+    min_size: usize,
     chunks: [AtomicPtr<T>; MAX_CHUNKS],
     freeman: Mutex<FreeManager<T>>,
 }
@@ -33,43 +40,69 @@ pub struct Arena<T> {
 impl<T> ops::Index<Handle<T>> for Arena<T> {
     type Output = T;
     fn index(&self, handle: Handle<T>) -> &T {
-        let first_ptr = &self.chunks[handle.0.chunk as usize].load(Ordering::Acquire);
-        unsafe { &*first_ptr.add(handle.0.index.get() as usize) }
+        let first_ptr = &self.chunks[handle.0.chunk.get() as usize].load(Ordering::Acquire);
+        unsafe { &*first_ptr.add(handle.0.index as usize) }
     }
 }
 
 impl<T: Default> Arena<T> {
-    pub fn new(init_size: usize) -> Self {
+    pub fn new(min_size: usize) -> Self {
+        assert_ne!(min_size, 0);
+        let dummy_data = Some(T::default()).into_iter().collect::<Box<[T]>>();
         Self {
-            init_size,
+            min_size,
             chunks: Default::default(),
-            freeman: Mutex::default(),
+            freeman: Mutex::new(FreeManager {
+                chunk_bases: vec![Box::into_raw(dummy_data)],
+                free_list: Vec::new(),
+            }),
         }
     }
 
-    pub fn alloc(&self) -> Handle<T> {
+    pub fn alloc(&self, value: T) -> Handle<T> {
         let mut freeman = self.freeman.lock().unwrap();
-        let address = match freeman.free_list.pop() {
-            Some(address) => address,
+        let (address, chunk_start) = match freeman.free_list.pop() {
+            Some(address) => {
+                let chunk_start = self.chunks[address.chunk.get() as usize].load(Ordering::Acquire);
+                (address, chunk_start)
+            }
             None => {
                 let address = Address {
-                    index: NonZeroU32::new(1).unwrap(),
-                    chunk: freeman.chunk_bases.len() as _,
+                    index: 0,
+                    chunk: NonZeroU8::new(freeman.chunk_bases.len() as _).unwrap(),
                 };
-                let size = self.init_size << freeman.chunk_bases.len();
+                let size = self.min_size << freeman.chunk_bases.len();
                 let mut data = (0..size).map(|_| T::default()).collect::<Box<[T]>>();
-                self.chunks[address.chunk as usize]
-                    .store(data.first_mut().unwrap(), Ordering::Release);
+                let chunk_start = data.first_mut().unwrap() as *mut T;
+                self.chunks[address.chunk.get() as usize].store(chunk_start, Ordering::Release);
                 freeman.chunk_bases.push(Box::into_raw(data));
-                address
+                (address, chunk_start)
             }
         };
+        unsafe {
+            ptr::write(chunk_start.add(address.index as usize), value);
+        }
         Handle(address, PhantomData)
+    }
+}
+
+impl<T> Drop for FreeManager<T> {
+    fn drop(&mut self) {
+        for base in self.chunk_bases.drain(..) {
+            let _ = unsafe { Box::from_raw(base) };
+        }
     }
 }
 
 // Ensure the `Option<Handle>` doesn't have any overhead
 #[cfg(test)]
-unsafe fn test_option_handle<A>(handle: Handle<A>) -> Option<Handle<A>> {
+unsafe fn _test_option_handle<A>(handle: Handle<A>) -> Option<Handle<A>> {
     std::mem::transmute(handle)
+}
+
+#[test]
+fn test_single_thread() {
+    let arena = Arena::<usize>::new(1);
+    let _ = arena.alloc(3);
+    let _ = arena.alloc(4);
 }
--- blade-asset/src/lib.rs
+++ blade-asset/src/lib.rs
@@ -1,18 +1,75 @@
-use std::{mem, path::PathBuf};
+use std::{
+    collections::hash_map::{Entry, HashMap},
+    ops,
+    path::{Path, PathBuf},
+    sync::Mutex,
+};
 
 mod arena;
 
-pub use arena::Handle;
+pub struct Handle<A> {
+    inner: arena::Handle<Slot<A>>,
+    generation: u32,
+}
+impl<A> Clone for Handle<A> {
+    fn clone(&self) -> Self {
+        Handle {
+            inner: self.inner,
+            generation: self.generation,
+        }
+    }
+}
+impl<A> Copy for Handle<A> {}
 
+#[derive(Default)]
 struct Slot<A> {
     load_task: Option<choir::RunningTask>,
-    iteration: usize,
+    generation: u32,
     asset: A,
 }
 
 struct AssetManager<A> {
     root: PathBuf,
     slots: arena::Arena<Slot<A>>,
+    paths: Mutex<HashMap<PathBuf, Handle<A>>>,
 }
 
-impl<A> AssetManager<A> {}
+impl<A> ops::Index<Handle<A>> for AssetManager<A> {
+    type Output = A;
+    fn index(&self, handle: Handle<A>) -> &A {
+        let slot = &self.slots[handle.inner];
+        assert_eq!(handle.generation, slot.generation);
+        &slot.asset
+    }
+}
+
+impl<A: Default> AssetManager<A> {
+    pub fn new(root: &Path) -> Self {
+        Self {
+            root: root.to_path_buf(),
+            slots: arena::Arena::new(64),
+            paths: Mutex::default(),
+        }
+    }
+
+    pub fn load(&self, path: &Path, choir: &choir::Choir) -> (Handle<A>, &choir::RunningTask) {
+        let mut paths = self.paths.lock().unwrap();
+        let handle = match paths.entry(path.to_path_buf()) {
+            Entry::Occupied(e) => *e.get(),
+            Entry::Vacant(e) => {
+                let generation = 0;
+                let handle = self.slots.alloc(Slot {
+                    load_task: None, //TODO
+                    generation,
+                    asset: A::default(),
+                });
+                *e.insert(Handle {
+                    inner: handle,
+                    generation,
+                })
+            }
+        };
+        let task = self.slots[handle.inner].load_task.as_ref().unwrap();
+        (handle, task)
+    }
+}
