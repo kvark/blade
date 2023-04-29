74d9b47d81e91dff803a77437ca353f4e9f3e023
Dzmitry Malyshau <kvark@fastmail.com>
2023-04-29 14:40:26 -0700

    Serialization infra for baked data

--- blade-asset/tests/main.rs
+++ blade-asset/tests/main.rs
@@ -1,9 +1,13 @@
-use std::path::{Path, PathBuf};
+use std::{
+    fmt,
+    io::Read,
+    path::{Path, PathBuf},
+};
 
 struct Baker;
 impl blade_asset::Baker for Baker {
     type Meal = usize;
-    fn cook(&self, _input: &[u8]) -> Box<[u8]> {
+    fn cook(&self, _input: impl Read, _path: &Path) -> Box<[u8]> {
         (0..1).map(|_| 2u8).collect()
     }
     fn serve(&self, cooked: &[u8]) -> usize {
@@ -20,4 +24,17 @@ fn test_asset() {
     let (handle, task) = am.load(Path::new("Cargo.toml"));
     task.clone().join();
     assert_eq!(am[handle], 1);
+}
+
+fn flat_roundtrip<F: blade_asset::Flat + PartialEq + fmt::Debug>(data: F) {
+    let mut vec = vec![0u8; data.size()];
+    unsafe { data.write(vec.as_mut_ptr()) };
+    let other = unsafe { F::read(vec.as_ptr()) };
+    assert_eq!(data, other);
+}
+
+#[test]
+fn test_flatten() {
+    flat_roundtrip([0u32, 1u32, 2u32]);
+    flat_roundtrip(&[2u32, 4u32, 6u32][..]);
 }
