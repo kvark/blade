e9bcdecbcd091af487717cb474cd797c0a451aba
Dzmitry Malyshau <kvark@fastmail.com>
2024-04-03 00:04:47 -0700

    gles: use explicit bindings

--- blade-graphics/src/gles/pipeline.rs
+++ blade-graphics/src/gles/pipeline.rs
@@ -8,23 +8,86 @@ impl super::Context {
         group_layouts: &[&crate::ShaderDataLayout],
         vertex_fetch_states: &[crate::VertexFetchState],
         name: &str,
+        extra_flags: glsl::WriterFlags,
     ) -> super::PipelineInner {
         let gl = self.lock();
-        let use_explicit_bindings = false; //TODO
+        let force_explicit_bindings = self
+            .capabilities
+            .contains(super::Capabilities::BUFFER_STORAGE);
+        let mut naga_options = glsl::Options {
+            version: glsl::Version::Embedded {
+                version: if force_explicit_bindings { 320 } else { 300 },
+                is_webgl: cfg!(target_arch = "wasm32"),
+            },
+            writer_flags: extra_flags | glsl::WriterFlags::ADJUST_COORDINATE_SPACE,
+            binding_map: Default::default(),
+            zero_initialize_workgroup_memory: false,
+        };
+
+        let mut group_mappings = group_layouts
+            .iter()
+            .map(|layout| super::ShaderDataMapping {
+                targets: vec![Vec::new(); layout.bindings.len()].into_boxed_slice(),
+            })
+            .collect::<Box<[_]>>();
+        if force_explicit_bindings {
+            let mut num_textures = 0u32;
+            let mut num_samplers = 0u32;
+            let mut num_buffers = 0u32;
+            for (group_index, (data_mapping, &layout)) in group_mappings
+                .iter_mut()
+                .zip(group_layouts.iter())
+                .enumerate()
+            {
+                for (binding_index, (slot_list, &(_, ref binding))) in data_mapping
+                    .targets
+                    .iter_mut()
+                    .zip(layout.bindings.iter())
+                    .enumerate()
+                {
+                    let target = match *binding {
+                        crate::ShaderBinding::Texture => {
+                            num_textures += 1;
+                            num_textures - 1
+                        }
+                        crate::ShaderBinding::Sampler => {
+                            num_samplers += 1;
+                            num_samplers - 1
+                        }
+                        crate::ShaderBinding::Buffer => {
+                            num_buffers += 1;
+                            num_buffers - 1
+                        }
+                        crate::ShaderBinding::TextureArray { .. }
+                        | crate::ShaderBinding::BufferArray { .. }
+                        | crate::ShaderBinding::AccelerationStructure => unimplemented!(),
+                        crate::ShaderBinding::Plain { .. } => {
+                            num_buffers += 1;
+                            num_buffers - 1
+                        }
+                    };
+
+                    let rb = naga::ResourceBinding {
+                        group: group_index as u32,
+                        binding: binding_index as u32,
+                    };
+                    naga_options.binding_map.insert(rb, target as u8);
+                    slot_list.push(target);
+                }
+            }
+            log::info!(
+                "Detected {} textures, {} samples, and {} buffers",
+                num_textures,
+                num_samplers,
+                num_buffers
+            );
+        }
 
         let program = gl.create_program().unwrap();
         #[cfg(not(target_arch = "wasm32"))]
         if !name.is_empty() && gl.supports_debug() {
             gl.object_label(glow::PROGRAM, std::mem::transmute(program), Some(name));
         }
-
-        let naga_options = glsl::Options {
-            version: glsl::Version::Embedded {
-                version: if use_explicit_bindings { 320 } else { 300 },
-                is_webgl: cfg!(target_arch = "wasm32"),
-            },
-            ..Default::default()
-        };
 
         let mut baked_shaders = Vec::with_capacity(shaders.len());
         let mut group_infos = group_layouts
@@ -38,7 +101,7 @@ impl super::Context {
             let ep = &sf.shader.module.entry_points[ep_index];
 
             let mut module = sf.shader.module.clone();
-            if use_explicit_bindings {
+            if force_explicit_bindings {
                 let ep_info = sf.shader.info.get_entry_point(ep_index);
                 crate::Shader::fill_resource_bindings(
                     &mut module,
@@ -111,101 +174,101 @@ impl super::Context {
         assert!(linked_ok, "Link: {}", msg);
         gl.use_program(Some(program));
 
-        let mut group_mappings = group_layouts
-            .iter()
-            .map(|layout| super::ShaderDataMapping {
-                targets: vec![Vec::new(); layout.bindings.len()].into_boxed_slice(),
-            })
-            .collect::<Box<[_]>>();
-
-        let force_uniform_block_assignment = true;
-        let mut variables_to_bind = Vec::new();
-        for (sf, &(shader, ref reflection)) in shaders.iter().zip(baked_shaders.iter()) {
-            for (glsl_name, mapping) in reflection.texture_mapping.iter() {
-                variables_to_bind.push((glsl_name, mapping.texture));
-                if let Some(handle) = mapping.sampler {
+        if !force_explicit_bindings {
+            let force_uniform_block_assignment = true;
+            let mut variables_to_bind = Vec::new();
+            for (sf, &(_, ref reflection)) in shaders.iter().zip(baked_shaders.iter()) {
+                for (glsl_name, mapping) in reflection.texture_mapping.iter() {
+                    variables_to_bind.push((glsl_name, mapping.texture));
+                    if let Some(handle) = mapping.sampler {
+                        variables_to_bind.push((glsl_name, handle));
+                    }
+                }
+                for (&handle, glsl_name) in reflection.uniforms.iter() {
                     variables_to_bind.push((glsl_name, handle));
                 }
-            }
-            for (&handle, glsl_name) in reflection.uniforms.iter() {
-                variables_to_bind.push((glsl_name, handle));
-            }
 
-            for (glsl_name, var_handle) in variables_to_bind.drain(..) {
-                let var = &sf.shader.module.global_variables[var_handle];
-                let var_name = var.name.as_ref().unwrap().as_str();
-                let (group_index, binding_index) = group_layouts
-                    .iter()
-                    .enumerate()
-                    .find_map(|(group_index, layout)| {
-                        layout
-                            .bindings
-                            .iter()
-                            .position(|&(name, _)| name == var_name)
-                            .map(|binding_index| (group_index, binding_index))
-                    })
-                    .unwrap_or_else(|| {
-                        panic!("Shader variable {} is not found in the bindings", var_name)
-                    });
+                for (glsl_name, var_handle) in variables_to_bind.drain(..) {
+                    let var = &sf.shader.module.global_variables[var_handle];
+                    let var_name = var.name.as_ref().unwrap().as_str();
+                    let (group_index, binding_index) = group_layouts
+                        .iter()
+                        .enumerate()
+                        .find_map(|(group_index, layout)| {
+                            layout
+                                .bindings
+                                .iter()
+                                .position(|&(name, _)| name == var_name)
+                                .map(|binding_index| (group_index, binding_index))
+                        })
+                        .unwrap_or_else(|| {
+                            panic!("Shader variable {} is not found in the bindings", var_name)
+                        });
 
-                let targets = &mut group_mappings[group_index].targets[binding_index];
-                match group_layouts[group_index].bindings[binding_index].1 {
-                    crate::ShaderBinding::Texture | crate::ShaderBinding::Sampler => {
-                        if let Some(ref location) = gl.get_uniform_location(program, glsl_name) {
-                            let mut slots = [0i32];
-                            gl.get_uniform_i32(program, location, &mut slots);
-                            targets.push(slots[0] as u32);
+                    let targets = &mut group_mappings[group_index].targets[binding_index];
+                    match group_layouts[group_index].bindings[binding_index].1 {
+                        crate::ShaderBinding::Texture | crate::ShaderBinding::Sampler => {
+                            if let Some(ref location) = gl.get_uniform_location(program, glsl_name)
+                            {
+                                let mut slots = [0i32];
+                                gl.get_uniform_i32(program, location, &mut slots);
+                                targets.push(slots[0] as u32);
+                            }
                         }
-                    }
-                    crate::ShaderBinding::Buffer => {
-                        if let Some(index) = gl.get_shader_storage_block_index(program, glsl_name) {
-                            let params = gl.get_program_resource_i32(
-                                program,
-                                glow::SHADER_STORAGE_BLOCK,
-                                index,
-                                &[glow::BUFFER_BINDING],
-                            );
-                            targets.push(params[0] as u32);
+                        crate::ShaderBinding::Buffer => {
+                            if let Some(index) =
+                                gl.get_shader_storage_block_index(program, glsl_name)
+                            {
+                                let params = gl.get_program_resource_i32(
+                                    program,
+                                    glow::SHADER_STORAGE_BLOCK,
+                                    index,
+                                    &[glow::BUFFER_BINDING],
+                                );
+                                targets.push(params[0] as u32);
+                            }
                         }
-                    }
-                    crate::ShaderBinding::TextureArray { .. }
-                    | crate::ShaderBinding::BufferArray { .. }
-                    | crate::ShaderBinding::AccelerationStructure => {
-                        unimplemented!()
-                    }
-                    crate::ShaderBinding::Plain { size } => {
-                        if let Some(index) = gl.get_uniform_block_index(program, glsl_name) {
-                            let expected_size = gl.get_active_uniform_block_parameter_i32(
-                                program,
-                                index,
-                                glow::UNIFORM_BLOCK_DATA_SIZE,
-                            ) as u32;
-                            let rounded_up_size = super::round_up_uniform_size(size);
-                            assert!(
-                                expected_size <= rounded_up_size,
-                                "Shader expects block[{}] size {}, but data has size of {} (rounded up to {})",
-                                index,
-                                expected_size,
-                                size,
-                                rounded_up_size,
-                            );
-                            let slot = if force_uniform_block_assignment {
-                                gl.uniform_block_binding(program, index, index);
-                                index
-                            } else {
-                                gl.get_active_uniform_block_parameter_i32(
+                        crate::ShaderBinding::TextureArray { .. }
+                        | crate::ShaderBinding::BufferArray { .. }
+                        | crate::ShaderBinding::AccelerationStructure => {
+                            unimplemented!()
+                        }
+                        crate::ShaderBinding::Plain { size } => {
+                            if let Some(index) = gl.get_uniform_block_index(program, glsl_name) {
+                                let expected_size = gl.get_active_uniform_block_parameter_i32(
                                     program,
                                     index,
-                                    glow::UNIFORM_BLOCK_BINDING,
-                                ) as u32
-                            };
-                            targets.push(slot);
+                                    glow::UNIFORM_BLOCK_DATA_SIZE,
+                                ) as u32;
+                                let rounded_up_size = super::round_up_uniform_size(size);
+                                assert!(
+                                    expected_size <= rounded_up_size,
+                                    "Shader expects block[{}] size {}, but data has size of {} (rounded up to {})",
+                                    index,
+                                    expected_size,
+                                    size,
+                                    rounded_up_size,
+                                );
+                                let slot = if force_uniform_block_assignment {
+                                    gl.uniform_block_binding(program, index, index);
+                                    index
+                                } else {
+                                    gl.get_active_uniform_block_parameter_i32(
+                                        program,
+                                        index,
+                                        glow::UNIFORM_BLOCK_BINDING,
+                                    ) as u32
+                                };
+                                targets.push(slot);
+                            }
                         }
                     }
                 }
             }
 
-            gl.delete_shader(shader);
+            for (shader, _) in baked_shaders {
+                gl.delete_shader(shader);
+            }
         }
         gl.use_program(None);
 
@@ -222,18 +285,31 @@ impl super::Context {
     ) -> super::ComputePipeline {
         let wg_size = desc.compute.shader.module.entry_points[desc.compute.entry_point_index()]
             .workgroup_size;
-        let inner =
-            unsafe { self.create_pipeline(&[desc.compute], desc.data_layouts, &[], desc.name) };
+        let inner = unsafe {
+            self.create_pipeline(
+                &[desc.compute],
+                desc.data_layouts,
+                &[],
+                desc.name,
+                glsl::WriterFlags::empty(),
+            )
+        };
         super::ComputePipeline { inner, wg_size }
     }
 
     pub fn create_render_pipeline(&self, desc: crate::RenderPipelineDesc) -> super::RenderPipeline {
+        let extra_flags = if desc.primitive.topology == crate::PrimitiveTopology::PointList {
+            glsl::WriterFlags::FORCE_POINT_SIZE
+        } else {
+            glsl::WriterFlags::empty()
+        };
         let inner = unsafe {
             self.create_pipeline(
                 &[desc.vertex, desc.fragment],
                 desc.data_layouts,
                 desc.vertex_fetches,
                 desc.name,
+                extra_flags,
             )
         };
         super::RenderPipeline {
